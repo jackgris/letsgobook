fc66eb157bf70ad3c87d4f9d5959cddbc9325763
Gabriel Pozo <jackgris2@gmail.com>
2024-02-10 19:56:53 -0300

    add: end-to-end test and refactor tests
    Signed-off-by: Gabriel Pozo <jackgris2@gmail.com>

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,7 @@ func (app *application) routes() http.Handler {
 
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
 	// Unprotected application routes using the "dynamic" middleware chain.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
